f0f7015b7ccbc482011503fec00d51d660be468a
mohammadreza <mohammadrezabitarafan7@gmail.com>
2024-01-16 15:06:45 +0330

    1

--- src/Page/Home/home.js
+++ src/Page/Home/home.js
@@ -2,7 +2,11 @@ import React from "react";
 import MiniDrawer from "../../Ui/Drawer/drawer";
 const Home = () => {
 
+    console.log("Hello")
+
     return (
+
+        
 
 
         <React.Fragment>
